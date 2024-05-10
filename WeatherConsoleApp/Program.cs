85e126dd0bcb9a08827ac1a29e57001c5667f852
BachoKartsivadze <bachoqarcivadze62@gmail.com>
2024-05-09 23:09:51 -0700

    before add using

--- WeatherConsoleApp/Program.cs
+++ WeatherConsoleApp/Program.cs
@@ -5,41 +5,53 @@ using System.Net;
 using WeatherConsoleApp;
 
 
-IConfigurationRoot configuration = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
-var API_key = configuration.GetValue<string>("APIKey");
+public class program {
 
-// enter city
-Console.WriteLine("enter the city");
-string city = Console.ReadLine();
+    public static async Task Main(string[] args) {
 
-using (HttpClient client = new HttpClient())
-{
-    // create url
-    string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric";
+        IConfigurationRoot configuration = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
+        var API_key = configuration.GetValue<string>("APIKey");
 
-    // Send a GET request asynchronously and read the response as a string
-    var response = await client.GetAsync(url);
-    var json = await response.Content.ReadAsStringAsync();
+        // enter city
+        Console.WriteLine("enter the city");
+        string city = Console.ReadLine();
 
-    // Deserialize the JSON response into your WeatherInfo class
-    WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+        using (HttpClient client = new HttpClient())
+        {
+            // create url
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric";
 
-    // Output result
-    Console.WriteLine($"coordinates = {Info.coord.lon}, {Info.coord.lat}");
-    Console.WriteLine($"weather = {Info.weather[0].main}");
-    Console.WriteLine($"temperature = {Info.main.temp} F");
-    Console.WriteLine($"pressure = {Info.main.pressure} P");
-    Console.WriteLine($"humidity = {Info.main.humidity}");
+            // Send a GET request asynchronously and read the response as a string
+            var response = await client.GetAsync(url);
+            var json = await response.Content.ReadAsStringAsync();
 
-    int temperatureInfarenheits = convertTofarenheits(Info.main.temp);
-    Console.WriteLine($"temperature in farenheit is {temperatureInfarenheits}F");
+            // Deserialize the JSON response into your WeatherInfo class
+            WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+
+            // Output result
+            Console.WriteLine($"coordinates = {Info.coord.lon}, {Info.coord.lat}");
+            Console.WriteLine($"weather = {Info.weather[0].main}");
+            Console.WriteLine($"temperature = {Info.main.temp} F");
+            Console.WriteLine($"pressure = {Info.main.pressure} P");
+            Console.WriteLine($"humidity = {Info.main.humidity}");
+
+            int temperatureInfarenheits = convertTofarenheits(Info.main.temp);
+            Console.WriteLine($"temperature in farenheit is {temperatureInfarenheits}F");
+        }
+
+    }
+
+    public static int convertTofarenheits(double celsiusValue)
+    {
+        double farenheitValue = 0;
+        farenheitValue = (celsiusValue * 1.8) + 32;
+        return (int)farenheitValue;
+    }
 }
 
-int convertTofarenheits(double celsiusValue) {
-    double farenheitValue = 0;
-    farenheitValue = (celsiusValue * 1.8) + 32;
-    return (int)farenheitValue;
-}
+
+
+
 
 
 
---
+++ WeatherConsoleApp/converterMetods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherConsoleApp
+{
+    internal class converterMetods
+    {
+
+        int convertTofarenheits(double celsiusValue)
+        {
+            double farenheitValue = 0;
+            farenheitValue = (celsiusValue * 1.8) + 32;
+            return (int)farenheitValue;
+        }
+
+    }
+}
